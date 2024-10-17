5205a2a8eecc3a086e0208ef4bc588d08b6f10d3
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2024-10-17 07:47:24 +0000

    v2 - tout dans des values yaml

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 set -e
 
-helm upgrade --install hasher ./generic-chart \
-  --set image.repository=dockercoins/hasher \
-  #
-
-helm upgrade --install redis ./generic-chart \
-  --set image.repository=redis \
-  --set image.tag=latest \
-  --set service.port=6379 \
-  #
-
-helm upgrade --install rng ./generic-chart \
-  --set image.repository=dockercoins/rng \
-  #
-
-helm upgrade --install webui ./generic-chart \
-  --set image.repository=dockercoins/webui \
-  --set service.type=NodePort \
-  #
-
-helm upgrade --install worker ./generic-chart \
-  --set image.repository=dockercoins/worker \
-  #
+for COMPONENT in hasher redis rng webui worker;
+do
+  helm upgrade --install $COMPONENT ./generic-chart --values $COMPONENT.yaml
+done
